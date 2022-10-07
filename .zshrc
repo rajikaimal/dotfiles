bfd4844e609f0addcc47f3936f04f9a529361670
rajikaimal <inbox.rajika@gmail.com>
2022-10-07 17:15:22 +0800

    update bootstrap script

--- .zshrc
+++ .zshrc
@@ -4,7 +4,12 @@ export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="robbyrussell"
 
-plugins=(git)
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+plugins=(
+    git
+    zsh-autosuggestions
+)
 
 source $HOME/.aliases
 source $ZSH/oh-my-zsh.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,43 @@
-echo "Bootstrapping dotfiles"
+#!/usr/bin/zsh
+
+echo "--- bootstrapping dotfiles ---"
+
+OHMYZSH=/Users/rajikai/.oh-my-zsh
+ZSHRC=$HOME/.zshrc
+ALIASES=$HOME/.aliases
+GITCONFIG=$HOME/.gitconfig
+GITIGNORE=$HOME/.gitignore
+
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -e "$OHMYZSH" ]; then
+    echo "installing oh-my-zsh ..."
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+
+echo $ZSHRC
+
+if [ ! -e "$ZSHRC" ]; then
+    echo "linking .zhsrc ..."
+    ln .zshrc $ZSHRC
+fi
+
+if [ ! -e "$ALIASES" ]; then
+    echo "Linking .aliases ..."
+    ln .aliases $ALIASES
+fi
+
+if [ ! -e "$GITCONFIG" ]; then
+    echo "linking .gitconfig ..."
+    ln .gitconfig $GITCONFIG
+fi
+
+if [ ! -e "$GITIGNORE" ]; then
+    echo "linking .gitignore ..."
+    ln .gitignore $GITIGNORE
+fi
+
+echo "syncing ..."
+source $ZSHRC
+echo "exit 0"
